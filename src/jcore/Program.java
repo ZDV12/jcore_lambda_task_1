d755ebd06cf409798d4ae3034fbcfc4429961899
zhardennsk <zhardennsk@outlook.com>
2022-04-12 21:11:32 +0700

    Initial commit

---
+++ src/jcore/Program.java
@@ -0,0 +1,19 @@
+package jcore;
+
+public class Program {
+    public static void main (String []args){
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1,1);
+        int c = calc.devide.apply(a, b);
+
+        int q = calc.pow.apply(a);
+        int w = calc.abs.apply(a);
+        boolean e = calc.isPositive.test(a);
+
+
+        calc.println.accept(q);
+
+    }
+}
